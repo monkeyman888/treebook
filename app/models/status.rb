51a3abb0a1f7b8c6690eded82b8a08856db42649
randycwang <randycwang@69.148.200.192.client.dyn.strong-sf70.as22781.net>
2014-01-18 16:54:24 +0800

    added test for user first name, last name and profile correctness

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,4 @@
 class Status < ActiveRecord::Base
-	attr_accessible :content, :user_id
+	attr_accessible :content, :user_id # doesn't work when removing this line even though we have code in the status controller for permit; think it has to do with "protected" vs "private" and the code is different between the status and app/user controller
 	belongs_to :user # using this ruby command assumes status has field user_id and foreign key matches up with id key; adds a "user" property to our Status object
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
+  # removing this line seems to work ok while removing attr_accessible in status.rb does not so has to do with the code we used for .permit; in Rails4, "Strong Parameters" are used so no need for attr_accessible anymore 
+  #attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true, uniqueness: true, 
+  format: { 
+  	with: /a-zA-Z0-9_-/,
+  	message: "Must be formatted correctly."
+  }
+
 
   has_many :statuses
 
