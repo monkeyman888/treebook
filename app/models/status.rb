fc4eb13b93caa656ad5cdbba62a20ed9becb8089
randycwang <randycwang@50.148.200.192.client.dyn.strong-sf70.as22781.net>
2014-01-21 17:44:33 +0800

    added validation for user id and content fields

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,9 @@
 class Status < ActiveRecord::Base
 	attr_accessible :content, :user_id # doesn't work when removing this line even though we have code in the status controller for permit; think it has to do with "protected" vs "private" and the code is different between the status and app/user controller
 	belongs_to :user # using this ruby command assumes status has field user_id and foreign key matches up with id key; adds a "user" property to our Status object
+
+	validates :content, presence: true,
+			length: { minimum: 2 }
+
+	validates :user_id, presence: true		
 end
