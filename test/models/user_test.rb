c84ef2e00856692662873651d6414ef6d85dcd85
randycwang <randycwang@82.148.200.192.client.dyn.strong-sf70.as22781.net>
2014-01-23 17:25:03 +0800

    generated profile controller

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,7 +32,9 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-  	user = User.new
+    user = User.new(first_name: 'Jason', last_name: 'Seifer', email: 'jason2@teamtreehouse.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
   	user.profile_name = "My Profile With Spaces"
 
   	assert !user.save
